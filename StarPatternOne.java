5079a67cad3bc17db9ef70709c997b3dcbdde49c
AGNEESWARIK7595 <agneeswari.kcse@gmail.com>
2024-04-11 08:56:44 +0530

    Create StarPatternOne.java

---
+++ StarPatternOne.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class StarPatternOne
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		for(int i =1;i<=5;i++){
+			switch(i){
+					case 1:
+					System.out.println("*");
+					break;
+					case 2:
+					System.out.println("**");
+					break;
+					case 3:
+					System.out.println("***");
+					break;
+					case 4:
+					System.out.println("****");
+					break;
+					case 5:
+					System.out.println("*****");
+					break;
+			}
+			
+		}
+	}
+}
