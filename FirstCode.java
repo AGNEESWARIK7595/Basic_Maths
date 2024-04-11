bf049860713be6058217c44e236125f934b3bc59
AGNEESWARIK7595 <agneeswari.kcse@gmail.com>
2024-04-11 07:51:58 +0530

    Update FirstCode.java

--- FirstCode.java
+++ FirstCode.java
@@ -2,10 +2,7 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-// Please make sure not to delete the WorkAtTech class
-// You can create additional non-public classes as well
-// But the main method should be in the WorkAtTech class
-class WorkAtTech
+class FirstCode
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
