a922007f1dcddd99332ac8c33e7a1a14638bef9d
AGNEESWARIK7595 <agneeswari.kcse@gmail.com>
2024-04-11 07:52:40 +0530

    Update Onetoten.java

--- Onetoten.java
+++ Onetoten.java
@@ -2,10 +2,8 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-// Please make sure not to delete the WorkAtTech class
-// You can create additional non-public classes as well
-// But the main method should be in the WorkAtTech class
-class WorkAtTech
+
+class Onetoten
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
