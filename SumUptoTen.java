f114a33b2958aa2f543673dd5d0625e9a7c5709f
AGNEESWARIK7595 <agneeswari.kcse@gmail.com>
2024-04-11 07:48:02 +0530

    Create SumUptoTen.java

---
+++ SumUptoTen.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// Please make sure not to delete the WorkAtTech class
+// You can create additional non-public classes as well
+// But the main method should be in the WorkAtTech class
+class WorkAtTech
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int output=0;
+		for(int i=0;i<=10;i++){
+			output = output +i;
+		
+		}
+		System.out.println(output);
+	}
+}
