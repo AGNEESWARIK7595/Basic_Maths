90d73b3658cec772280bea384f6b65d56cea85d6
AGNEESWARIK7595 <agneeswari.kcse@gmail.com>
2024-04-11 15:05:40 +0530

    Create SquareSum.java

---
+++ SquareSum.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class SquareSum
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int total = sc.nextInt();
+		for(int i=0;i<total;i++){
+			int num = sc.nextInt();
+			int c=0,temp=0;
+			while(num!=0){
+				c=num%10;
+				temp+=(c*c);
+				num=num/10;
+			}
+			System.out.println(temp);
+				}
+		}
+	
+}
